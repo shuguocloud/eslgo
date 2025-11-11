2d0ced0774fef022aee7b1edc3ddfff7bccc218d
opensmarty <opensmarty@163.com>
2025-11-11 18:55:46 +0800

    1. Add some methods for mod_dptools with sync or async mode.

--- helper.go
+++ helper.go
@@ -54,98 +54,106 @@ func (c *Conn) DebugOff(id string) {
 }
 
 // Phrase - Executes the mod_dptools phrase app
-func (c *Conn) Phrase(ctx context.Context, uuid, macro string, times int, wait bool) (*RawResponse, error) {
-	return c.audioCommand(ctx, "phrase", uuid, macro, times, wait)
+func (c *Conn) Phrase(ctx context.Context, uuid, macro string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "phrase", uuid, macro, times, true)
+}
+
+// PhraseAsync - Executes the mod_dptools phrase app with async mode
+func (c *Conn) PhraseAsync(ctx context.Context, uuid, macro string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "phrase", uuid, macro, times, false)
 }
 
 // PhraseWithArg - Executes the mod_dptools phrase app with arguments
-func (c *Conn) PhraseWithArg(ctx context.Context, uuid, macro string, argument interface{}, times int, wait bool) (*RawResponse, error) {
-	return c.audioCommand(ctx, "phrase", uuid, fmt.Sprintf("%s,%v", macro, argument), times, wait)
+func (c *Conn) PhraseWithArg(ctx context.Context, uuid, macro string, argument interface{}, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "phrase", uuid, fmt.Sprintf("%s,%v", macro, argument), times, true)
+}
+
+// PhraseWithArgAsync - Executes the mod_dptools phrase app with arguments by async mode
+func (c *Conn) PhraseWithArgAsync(ctx context.Context, uuid, macro string, argument interface{}, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "phrase", uuid, fmt.Sprintf("%s,%v", macro, argument), times, false)
 }
 
 // Playback - Executes the mod_dptools playback app
-func (c *Conn) Playback(ctx context.Context, uuid, audioArgs string, times int, wait bool) (*RawResponse, error) {
-	return c.audioCommand(ctx, "playback", uuid, audioArgs, times, wait)
+func (c *Conn) Playback(ctx context.Context, uuid, audioArgs string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "playback", uuid, audioArgs, times, true)
+}
+
+// PlaybackAsync - Executes the mod_dptools playback app with async mode
+func (c *Conn) PlaybackAsync(ctx context.Context, uuid, audioArgs string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "playback", uuid, audioArgs, times, false)
 }
 
 // Say - Executes the mod_dptools say app
-func (c *Conn) Say(ctx context.Context, uuid, audioArgs string, times int, wait bool) (*RawResponse, error) {
-	return c.audioCommand(ctx, "say", uuid, audioArgs, times, wait)
+func (c *Conn) Say(ctx context.Context, uuid, audioArgs string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "say", uuid, audioArgs, times, true)
+}
+
+// SayAsync - Executes the mod_dptools say app with async mode
+func (c *Conn) SayAsync(ctx context.Context, uuid, audioArgs string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "say", uuid, audioArgs, times, false)
 }
 
 // Speak - Executes the mod_dptools speak app
-func (c *Conn) Speak(ctx context.Context, uuid, audioArgs string, times int, wait bool) (*RawResponse, error) {
-	return c.audioCommand(ctx, "speak", uuid, audioArgs, times, wait)
+func (c *Conn) Speak(ctx context.Context, uuid, audioArgs string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "speak", uuid, audioArgs, times, true)
+}
+
+// SpeakAsync - Executes the mod_dptools speak app with async mode
+func (c *Conn) SpeakAsync(ctx context.Context, uuid, audioArgs string, times int) (*RawResponse, error) {
+	return c.audioCommand(ctx, "speak", uuid, audioArgs, times, false)
 }
 
 // Execute - Executes the mod_dptools app
-func (c *Conn) Execute(ctx context.Context, uuid, command, appArgs string, wait bool) (*RawResponse, error) {
-	response, err := c.SendCommand(ctx, &call.Execute{
-		UUID:    uuid,
-		AppName: command,
-		AppArgs: appArgs,
-		Sync:    wait,
-	})
-	if err != nil {
-		return response, err
-	}
-	if !response.IsOk() {
-		return response, errors.New(command + " response is not okay")
-	}
-	return response, nil
+func (c *Conn) Execute(ctx context.Context, uuid, command, appArgs string) (*RawResponse, error) {
+	return c.executeCommand(ctx, command, uuid, appArgs, true)
+}
+
+// ExecuteAsync - Executes the mod_dptools app with async mode
+func (c *Conn) ExecuteAsync(ctx context.Context, uuid, command, appArgs string) (*RawResponse, error) {
+	return c.executeCommand(ctx, command, uuid, appArgs, false)
 }
 
 // Hangup - Executes the mod_dptools hangup app
-func (c *Conn) Hangup(ctx context.Context, uuid, cause string, wait bool) (*RawResponse, error) {
-	response, err := c.SendCommand(ctx, call.Hangup{
-		UUID:  uuid,
-		Cause: cause,
-		Sync:  wait,
-	})
-	if err != nil {
-		return response, err
-	}
-	if !response.IsOk() {
-		return response, errors.New("set response is not okay")
-	}
-	return response, nil
+func (c *Conn) Hangup(ctx context.Context, uuid, cause string) (*RawResponse, error) {
+	return c.hangupCommand(ctx, uuid, cause, true)
+}
+
+// HangupAsync - Executes the mod_dptools hangup app with async mode
+func (c *Conn) HangupAsync(ctx context.Context, uuid, cause string) (*RawResponse, error) {
+	return c.hangupCommand(ctx, uuid, cause, false)
 }
 
 // Answer - Executes the mod_dptools answer app
-func (c *Conn) Answer(ctx context.Context, uuid string, wait bool) (*RawResponse, error) {
-	return c.executeCommand(ctx, "answer", uuid, "", wait)
+func (c *Conn) Answer(ctx context.Context, uuid string) (*RawResponse, error) {
+	return c.executeCommand(ctx, "answer", uuid, "", true)
+}
+
+// AnswerAsync - Executes the mod_dptools answer app with async mode
+func (c *Conn) AnswerAsync(ctx context.Context, uuid string) (*RawResponse, error) {
+	return c.executeCommand(ctx, "answer", uuid, "", false)
 }
 
 // Set - Executes the mod_dptools set app
-func (c *Conn) Set(ctx context.Context, uuid, key, value string, wait bool) (*RawResponse, error) {
-	response, err := c.SendCommand(ctx, &call.Set{
-		UUID:  uuid,
-		Key:   key,
-		Value: value,
-		Sync:  wait,
-	})
-	if err != nil {
-		return response, err
-	}
-	if !response.IsOk() {
-		return response, errors.New("set response is not okay")
-	}
-	return response, nil
+func (c *Conn) Set(ctx context.Context, uuid, key, value string) (*RawResponse, error) {
+	return c.setCommand(ctx , uuid, key, value, true)
 }
 
-// Export - Executes the mod_dptools export app
-func (c *Conn) Export(ctx context.Context, uuid, key, value string, wait bool) (*RawResponse, error) {
-	response, err := c.SendCommand(ctx, &call.Export{
-		UUID:  uuid,
-		Key:   key,
-		Value: value,
-		Sync:  wait,
+// SetAsync - Executes the mod_dptools set app with async mode
+func (c *Conn) SetAsync(ctx context.Context, uuid, key, value string)(*RawResponse, error)   {
+	return c.setCommand(ctx , uuid, key, value, false)
+}
+
+// Linger - Executes the mod_dptools linger app
+func (c *Conn) Linger(ctx context.Context, enabled bool, time time.Duration) (*RawResponse, error) {
+	response, err := c.SendCommand(ctx, command.Linger{
+		Enabled: enabled,
+		Seconds: time,
 	})
 	if err != nil {
 		return response, err
 	}
 	if !response.IsOk() {
-		return response, errors.New("export response is not okay")
+		return response, errors.New("linger response is not okay")
 	}
 	return response, nil
 }
@@ -212,4 +220,36 @@ func (c *Conn) audioCommand(ctx context.Context, command, uuid, audioArgs string
 // Helper fuck to execute commands with its args and sync/async mode
 func (c *Conn) executeCommand(ctx context.Context, command, uuid, audioArgs string, wait bool) (*RawResponse, error) {
 	return c.audioCommand(ctx, command, uuid, audioArgs, 0, wait)
+}
+
+// Helper fuck to set commands with its args and sync/async mode
+func (c *Conn) setCommand(ctx context.Context, uuid, key, value string, wait bool) (*RawResponse, error) {
+	response, err := c.SendCommand(ctx, &call.Set{
+		UUID:  uuid,
+		Key:   key,
+		Value: value,
+		Sync:  wait,
+	})
+	if err != nil {
+		return response, err
+	}
+	if !response.IsOk() {
+		return response, errors.New("set response is not okay")
+	}
+	return response, nil
+}
+
+func (c *Conn) hangupCommand(ctx context.Context, uuid, cause string, wait bool) (*RawResponse, error) {
+	response, err := c.SendCommand(ctx, call.Hangup{
+		UUID:  uuid,
+		Cause: cause,
+		Sync:  wait,
+	})
+	if err != nil {
+		return response, err
+	}
+	if !response.IsOk() {
+		return response, errors.New("set response is not okay")
+	}
+	return response, nil
 }
